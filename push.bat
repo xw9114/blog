956234e8267cac0fef8ab612a08f542b456bdcd7
XW9114 <1989066288@qq.com>
2026-04-21 21:10:10 +0800

    1

--- push.bat
+++ push.bat
@@ -1,9 +1,37 @@
 @echo off
-cd /d D:\blog
-git add .
+setlocal
+
+cd /d "D:\blog" || goto :error
+
+for /f "delims=" %%i in ('git rev-parse --abbrev-ref HEAD 2^>nul') do set "branch=%%i"
+if not defined branch goto :error
+
+for /f %%i in ('git status --porcelain ^| find /c /v ""') do set "changes=%%i"
+if "%changes%"=="0" (
+    echo No changes to commit.
+    goto :end
+)
+
 set /p msg=Commit message: 
+if "%msg%"=="" set "msg=update blog"
+
+git add .
+if errorlevel 1 goto :error
+
 git commit -m "%msg%"
-git push
+if errorlevel 1 goto :error
+
+git push origin "%branch%"
+if errorlevel 1 goto :error
+
 echo.
-echo Done!
+echo Done! Pushed to origin/%branch%.
+goto :end
+
+:error
+echo.
+echo Push failed. Please check git output above.
+
+:end
 pause
+endlocal
